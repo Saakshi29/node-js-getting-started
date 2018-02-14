8d7dc85ab994d10ef50335be1c60a6dc02209c2f
Saakshi <saakshi.gupta2910@gmail.com>
2018-02-15 00:26:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+.post('/form', function(req, res){
+    
+	
+	
+	response = "This is a sample response from your webhook!"; //Default response from the webhook to show it's working
+
+	res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+   // console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+});
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
